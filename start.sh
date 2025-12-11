52f5a1f7d5c0e105fb14b7ddcf86270ac541aa0c
Peter Havekes <peter@havekes.eu>
2025-12-11 15:13:54 +0100

    Add docker network

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 docker stop entra-sync-runner
 docker rm entra-sync-runner
 docker build -t entra-sync-scheduler .
-docker run -d --name entra-sync-runner -v ./config.yaml:/app/config.yaml:ro entra-sync-scheduler
+docker run -d --name entra-sync-runner -v ./config.yaml:/app/config.yaml:ro --network=openconext_mongodb entra-sync-scheduler
 docker exec -it entra-sync-runner /bin/sh -c "cd /app && php sync.php"
