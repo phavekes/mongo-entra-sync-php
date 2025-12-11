1951f324ead56b88bd9ab996527d714d7d0bf23e
Peter Havekes <peter@havekes.eu>
2025-12-11 15:08:34 +0100

    docker rm

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker stop entra-sync-runner
-docker remove entra-sync-runner
+docker rm entra-sync-runner
 docker build -t entra-sync-scheduler .
 docker run -d --name entra-sync-runner -v ./config.yaml:/app/config.yaml:ro entra-sync-scheduler
 docker exec -it entra-sync-runner /bin/sh -c "cd /app && php sync.php"
